9b12df7951a0b799631abd0f27276f15280f4065
halposhaven <matt.mattjones@gmail.com>
2013-11-16 17:41:36 -0500

    deploy hook correction

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,1 +1,1 @@
-if [ -f #{config.shared_path}/wp-config.php ];then ln -f #{config.shared_path}/wp-config.php #{config.current_path}/public/wp-config.php;fi
+run "if [ -f #{config.shared_path}/wp-config.php ];then ln -f #{config.shared_path}/wp-config.php #{config.current_path}/public/wp-config.php;fi"
