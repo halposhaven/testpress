98b9d5d054fd5fa3d37290706bfb8ad012e3554a
halposhaven <matt.mattjones@gmail.com>
2013-11-16 23:39:51 -0500

    deploy hook for uploads

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,1 +1,4 @@
+# symlink wp-config
 run "if [ -f #{config.shared_path}/wp-config.php ];then ln -f #{config.shared_path}/wp-config.php #{config.current_path}/public/wp-config.php;fi"
+# symlink uploads folder
+run "if [ -d #{config.shared_path}/uploads ]; then ln -f #{config.shared_path}/uploads #{config.current_path}/public/wp-content/uploads/;fi"
